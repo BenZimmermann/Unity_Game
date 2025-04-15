0425285d8d4d595ac9bb507270d96b30ff1a81e9
BenZimmermann <ben.eric.zimmermann@gmail.com>
2025-04-15 11:34:17 +0200

    UPD BaseCharacterController UPD Cinemachine

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -5,8 +5,16 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class BaseCharacterController : MonoBehaviour
 {
+
+    private Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
     public void Movement(CallbackContext ctx)
     {
-        Debug.Log($"Context: {ctx.ReadValue<Vector2>()}");
+       movementInput = ctx.ReadValue<Vector2>();
+    }
+    public void Update()
+    {
+        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
     }
 }
