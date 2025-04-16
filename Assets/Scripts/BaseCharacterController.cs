328b44cc2594613cac4ba27549e87c86457fd64b
BenZimmermann <ben.eric.zimmermann@gmail.com>
2025-04-16 13:26:30 +0200

    NEW assets NEW hitbox

--- Assets/Scripts/BaseCharacterController.cs
+++ Assets/Scripts/BaseCharacterController.cs
@@ -8,13 +8,32 @@ public class BaseCharacterController : MonoBehaviour
 
     private Vector2 movementInput;
     [SerializeField] float movementSpeed;
+    private bool isSlowed;
 
+    private void Start()
+    {
+        isSlowed = false;
+    }
     public void Movement(CallbackContext ctx)
     {
        movementInput = ctx.ReadValue<Vector2>();
     }
-    public void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed;
+        transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * Time.deltaTime * movementSpeed);
+    }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Swamp"))
+        {
+            isSlowed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Swamp"))
+        {
+            isSlowed = false;
+        }
     }
 }
